1ee0bddabf8d0108879cc979d38a2ad38d4eddab
Chris Meek <Chris@Amies-iMac.local>
2017-01-21 18:08:04 +0000

    added template routes

--- src/client/app.js
+++ src/client/app.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-class MyComp extends Component {
+import Home from './home'
+import NoMatch from './nomatch'
+
+injectTapEventPlugin();
+
+class App extends Component {
+    static propTypes = {
+        children: React.PropTypes.object.isRequired
+    };
+
     render() {
         return (
-            <h1>Hello World</h1>
+            <MuiThemeProvider>
+                {this.props.children}
+            </MuiThemeProvider>
         )
     }
 }
 
 ReactDOM.render(
-    <MyComp/>,
+    <Router history={browserHistory}>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home} />
+
+        </Route>
+        <Route path='*' component={NoMatch} />
+    </Router>,
     document.getElementById('root')
-)
+);
